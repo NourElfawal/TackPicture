706af0fb967b1e76ccdb497a91ea20a86feccefc
NourElfawal <nonoegyp@yahoo.com>
2017-04-20 01:44:02 +0200

    upload

---
+++ app/src/main/java/com/example/pc/tackpicture/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.pc.tackpicture;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnCam;
+    private static final int CAMERA_REQUEST =123 ;
+    ImageView imageViewCam;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCam = (Button) findViewById(R.id.cambtn);
+        imageViewCam = (ImageView) findViewById(R.id.imageViewCam);
+        btnCam.setOnClickListener(this);
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageViewCam.setImageBitmap(photo);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST);
+    }
+}
